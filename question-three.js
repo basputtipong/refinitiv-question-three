a4dfbfd1e1ef8ed20325d0aff241afb07ff0be33
basputtipong <basputtipongz@gmail.com>
2021-07-18 23:48:47 +0700

    [question-three] first commit.

---
+++ question-three.js
@@ -0,0 +1,16 @@
+var input = process.argv[2];
+
+switch (input) {
+  case "B-INCOMESSF":
+    console.log("10.0548");
+    break;
+  case "BM70SSF":
+    console.log("9.9774");
+    break;
+  case "BEQSSF":
+    console.log("11.247");
+    break;
+  case "B-FUTURESSF":
+    console.log("11.443");
+    break;
+}
